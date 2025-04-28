520adb7af90b4daa37556aefab990ec4c88f16a2
Daniel <moongames12345@gmail.com>
2025-04-28 08:47:29 -0300

    começo

---
+++ meu_site/src/scripts/app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Código para inicializar a interatividade do site
+    console.log('O site está pronto para interatividade!');
+    
+    document.getElementById('actionButton').addEventListener('click', () => {
+        alert('Botão clicado!');
+    });
+});
